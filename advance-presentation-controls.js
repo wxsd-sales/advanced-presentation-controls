8887f0e34429769fe4c5266abcd1f69a61a9f344
William Mills <wmpmills@gmail.com>
2023-04-25 20:14:14 +0200

    Update advance-presentation-controls.js

--- advance-presentation-controls.js
+++ advance-presentation-controls.js
@@ -1,16 +1,4 @@
 /********************************************************
-Copyright (c) 2022 Cisco and/or its affiliates.
-This software is licensed to you under the terms of the Cisco Sample
-Code License, Version 1.1 (the "License"). You may obtain a copy of the
-License at
-               https://developer.cisco.com/docs/licenses
-All use of the material herein must be in accordance with the terms of
-the License. All rights not expressly granted by the License are
-reserved. Unless required by applicable law or agreed to separately in
-writing, software distributed under the License is distributed on an "AS
-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-or implied.
-*********************************************************
  * 
  * Macro Author:      	William Mills
  *                    	Technical Solutions Specialist 
@@ -29,11 +17,14 @@ or implied.
  * Additionally it also gives you a greater control of local preview content
  * by automatically restoring a local preview which is normally stopped when
  * a call is made or a Cloud Proximity share has been started.
- * https://github.com/wxsd-sales/advance-presentation-controls
  * 
  * By default Webex Devices will auto preview a new video signal while not
  * on a call and can be and also can be configured to do the same while on a call. 
  * This feature will toggle the in call behaviour.
+ *
+ * Full Readme, source code and license agreement available on Github:
+ * https://github.com/wxsd-sales/advance-presentation-controls
+ *
  ********************************************************/
 
 import xapi from 'xapi';
